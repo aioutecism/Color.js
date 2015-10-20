8715501ad208b9f7f5ae27f1edcd0c23114182f5
aioute Gao <aioutecism@gmail.com>
2015-10-20 17:31:14 +0900

    Config gulp for build.

--- gulpfile.js
+++ gulpfile.js
@@ -6,33 +6,56 @@ var runSequence = require('run-sequence');
 var $ = require('gulp-load-plugins')();
 
 var PATH = {
-    src  : 'src',
-    dest : 'dist'
+    src   : 'src',
+    dev   : 'dev',
+    build : 'build'
 };
 
-gulp.task('clean', function () {
+// For development
+gulp.task('clean:dev', function () {
     return del([
-        PATH.dest
+        PATH.dev
     ]);
 });
 
-gulp.task('babel', function () {
+gulp.task('babel:dev', function () {
     return gulp.src(PATH.src + '/**/*.js')
         .pipe($.sourcemaps.init())
             .pipe($.babel())
-            .pipe($.uglify())
         .pipe($.sourcemaps.write())
-        .pipe(gulp.dest(PATH.dest));
+        .pipe(gulp.dest(PATH.dev));
 });
 
-gulp.task('watch', function () {
-    gulp.watch(PATH.src + '/**/*.js', ['babel']);
+gulp.task('dev', function (callback) {
+    return runSequence(
+        'clean:dev',
+        'babel:dev',
+        function () {
+            gulp.watch(PATH.src + '/**/*.js', ['babel:dev']);
+            callback && callback();
+        }
+    );
+});
+
+// For build
+gulp.task('clean:build', function () {
+    return del([
+        PATH.build
+    ]);
+});
+
+gulp.task('babel:build', function () {
+    return gulp.src(PATH.src + '/**/*.js')
+        .pipe($.babel())
+        .pipe($.uglify())
+        .pipe($.concat('color.min.js'))
+        .pipe(gulp.dest(PATH.build));
 });
 
 gulp.task('default', function (callback) {
     return runSequence(
-        'clean',
-        'babel',
+        'clean:build',
+        'babel:build',
         callback
     );
 });
